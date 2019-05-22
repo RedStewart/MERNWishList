75ca3878c66bae57a9068323383aa3fdb3a837ae
Reed Stewart <reedostewart@gmail.com>
2019-05-23 11:21:38 +1200

    Finished MERN playlist, fullstack app working now. Login, logout, and register all working with authentication

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -32,6 +32,74 @@ export const loadUser = () => (dispatch, getState) => {
         });
 };
 
+//register user
+export const register = ({ name, email, password }) => dispatch => {
+    //header
+    const config = {
+        headers: {
+            "Content-Type": "application/json"
+        }
+    };
+
+    const body = JSON.stringify({ name, email, password });
+
+    axios
+        .post("/api/users", body, config)
+        .then(res =>
+            dispatch({
+                type: REGISTER_SUCCESS,
+                payload: res.data
+            })
+        )
+        .catch(err => {
+            dispatch(
+                returnErrors(
+                    err.response.data,
+                    err.response.status,
+                    "REGISTER_FAIL"
+                )
+            );
+            dispatch({ type: REGISTER_FAIL });
+        });
+};
+
+export const login = ({ email, password }) => dispatch => {
+    //header
+    const config = {
+        headers: {
+            "Content-Type": "application/json"
+        }
+    };
+
+    const body = JSON.stringify({ email, password });
+
+    axios
+        .post("/api/auth", body, config)
+        .then(res =>
+            dispatch({
+                type: LOGIN_SUCCESS,
+                payload: res.data
+            })
+        )
+        .catch(err => {
+            dispatch(
+                returnErrors(
+                    err.response.data,
+                    err.response.status,
+                    "LOGIN_FAIL"
+                )
+            );
+            dispatch({ type: LOGIN_FAIL });
+        });
+};
+
+//logout
+export const logout = () => {
+    return {
+        type: LOGOUT_SUCCESS
+    };
+};
+
 export const tokenConfig = getState => {
     //get token from localstorage
     const token = getState().auth.token;

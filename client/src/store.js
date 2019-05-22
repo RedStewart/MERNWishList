3a1a3c546237f38f643fd4818f6746f39acc2723
Reed Stewart <reedostewart@gmail.com>
2019-05-23 11:42:46 +1200

    Store error

--- client/src/store.js
+++ client/src/store.js
@@ -5,14 +5,15 @@ import rootReducer from "./reducers";
 const initialState = {};
 
 const middleware = [thunk];
-/*
+
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
     rootReducer,
     initialState,
     composeEnhancers(applyMiddleware(...middleware))
 );
-*/
+
+/*
 const composeEnhancers =
     typeof window === "object" && window._REDUX_DEVTOOLS_EXTENSION_COMPOSE_
         ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
@@ -27,5 +28,5 @@ const store = createStore(
     initialState,
     composingMiddlewareAndDevTools
 );
-
+*/
 export default store;
